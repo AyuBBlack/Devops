94d14399a23352ac5fdd9d939e2e1c26bb48dbbc
Ayub <ayub.gadiev95@gmail.com>
2022-03-24 06:19:56 +0300

    new 2

--- DotNet/WinFormsApp/WinFormsApp/Form1.cs
+++ DotNet/WinFormsApp/WinFormsApp/Form1.cs
@@ -3,21 +3,29 @@ namespace WinFormsApp
 {
     public partial class Form1 : Form
     {
-        private readonly ReverseService _ReverseService = new();
+        readonly HttpClient client;
+
+
         public Form1()
         {
             InitializeComponent();
+            client = new HttpClient();
         }
 
-        private async void buttonReverse_Click(object sender, EventArgs e)
+        private void buttonReverse_Click(object sender, EventArgs e)
         {
-            var text = InputBox.Text;
+            var text = 0;
 
-            var result = await _ReverseService.GetReverseText(text);
+            var strQuery = $"https://localhost:7100/ReverseText?text={text}";
 
-            var resultConvertJson = JsonConvert.DeserializeObject<ReverseService>(result);
+            var response = client.GetAsync(strQuery).Result;
 
-            ResultBox.Text = resultConvertJson.ToString();
+            var resultStr = response.Content.ReadAsStringAsync().Result;
+
+            var result = JsonConvert.DeserializeObject<FuncRevers>(resultStr);
+
+            ResultBox.Text = result?.ReverseResult.ToString();
         }
     }
-}
+ }
+
