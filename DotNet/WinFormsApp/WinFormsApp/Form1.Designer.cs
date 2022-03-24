c71cbcae2950244fd6bacca05fc21bc702b799a7
Ayub <ayub.gadiev95@gmail.com>
2022-03-24 03:17:57 +0300

    bomm

---
+++ DotNet/WinFormsApp/WinFormsApp/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.input = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // submit
+            // 
+            this.submit.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.submit.Location = new System.Drawing.Point(429, 172);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(250, 47);
+            this.submit.TabIndex = 0;
+            this.submit.Text = "Развернуть";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.Button1_ClickAsync);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(173, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите текст :";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.input.Location = new System.Drawing.Point(173, 172);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(250, 47);
+            this.input.TabIndex = 2;
+            this.input.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // result
+            // 
+            this.result.Enabled = false;
+            this.result.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.result.Location = new System.Drawing.Point(173, 255);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(506, 47);
+            this.result.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.submit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button submit;
+        private Label label1;
+        private TextBox input;
+        private TextBox result;
+    }
+}
---
+++ DotNet/WinFormsApp/WinFormsApp/Reverse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp
+{
+    internal class Reverse
+    {
+        public string word { get; set; }
+    }
+}
